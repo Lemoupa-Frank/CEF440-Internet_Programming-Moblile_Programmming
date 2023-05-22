08d7f90f4cf73343df67db2d75fd8e75f2c93784
Lemoupa-Frank <frankmichel022@gmail.com>
2023-05-22 17:55:37 +0100

    Activities Initiated

---
+++ GETME001/app/src/main/java/com/example/getme001/Request_Ride.java
@@ -0,0 +1,14 @@
+package com.example.getme001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Request_Ride extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_ride);
+    }
+}
