698a64fe0052415bfaa6735c0e32c0d6fe40ed06
THEOPTIMIST <gideonetuge46@gmail.com>
2023-06-16 16:15:49 +0100

    support page

---
+++ GETME001/app/src/main/java/Activity/Activity/Support.java
@@ -0,0 +1,16 @@
+package Activity.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.getme001.R;
+
+public class Support extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_support);
+    }
+}
