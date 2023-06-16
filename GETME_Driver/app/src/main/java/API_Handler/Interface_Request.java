be8436f508bc6afe3aa5cc63abf07d33e54a056d
Lemoupa-Frank <frankmichel022@gmail.com>
2023-06-16 13:49:50 +0100

    Driver App updrate

---
+++ GETME_Driver/app/src/main/java/API_Handler/Interface_Request.java
@@ -0,0 +1,20 @@
+package API_Handler;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface Interface_Request {
+    @GET("JETME_PHP/deletable.php")
+    Call<PassengerModel> getPassenger();
+    //@header('Content-Type: application');
+    @POST("JETME/PHP/new_user.php")
+    @FormUrlEncoded
+    Call<Void> Send_Logs(@Field("Username") String Username,@Field("contact") String contact,@Field("Password") String Password);
+}
+//"JETMEPHP/PHP/new_user.php"
+//Call<Void> Send_Logs(@Body PassengerModel passenger);
+//http://192.168.43.81/
+//    @POST("JETME/PHP/new_user.php")
---
+++ GETME_Driver/app/src/main/java/API_Handler/Retrofit_Base_Class.java
@@ -0,0 +1,26 @@
+package API_Handler;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+//http://192.168.43.81/ Rosine
+//http://192.168.43.107/ Frank
+public class Retrofit_Base_Class {
+    public static String BASE_URL ="http://192.168.43.81/";
+    public static Retrofit retrofit;
+    private static final Gson gson = new GsonBuilder()
+            .setLenient()
+            .create();
+    public static Retrofit getClient(){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+        return retrofit;
+    }
+}
