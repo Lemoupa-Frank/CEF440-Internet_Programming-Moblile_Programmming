db1b93be0e26decc3d0c0549ec64131dc4f15432
Lemoupa-Frank <frankmichel022@gmail.com>
2023-06-16 16:19:38 +0100

    Modify SignUp Driver

--- GETME_Driver/app/src/main/java/Activity/Activity/Signup.java
+++ GETME_Driver/app/src/main/java/Activity/Activity/Signup.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,11 +13,10 @@ import com.example.getme_driver.R;
 
 public class Signup extends AppCompatActivity {
     TextView LOG; Button signup;
-
+    EditText fname,lname,phone_num,companyname,licence_nu,carplate, enterpass, confirmpass;
+    String Sfname,Slname,Sphone_num,Scompanyname,Slicence_nu,Scarplate, Senterpass, Sconfirmpass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        EditText fname,lname,phone_num,companyname,licence_nu,carplate, enterpass, confirmpass;
-        String Sfname,Slname,Sphone_num,Scompanyname,Slicence_nu,Scarplate, Senterpass, Sconfirmpass;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
         fname = findViewById(R.id.firstname);
@@ -30,15 +28,6 @@ public class Signup extends AppCompatActivity {
         enterpass = findViewById(R.id.enter_pass);
         confirmpass = findViewById(R.id.confirmpass);
 
-        Sfname = fname.getText().toString();
-        Slname = lname.getText().toString();
-        Sphone_num = phone_num.getText().toString();
-        Scompanyname = companyname.getText().toString();
-        Slicence_nu = licence_nu.getText().toString();
-        Scarplate = carplate.getText().toString();
-        Senterpass = enterpass.getText().toString();
-        Sconfirmpass = confirmpass.getText().toString();
-
         // Find the "Sign up" button and add a click listener
         signup = findViewById(R.id.signupbtn);
         LOG= findViewById(R.id.log);
@@ -49,10 +38,18 @@ public class Signup extends AppCompatActivity {
         });
         signup.setOnClickListener( s ->
         {
-            if(Sfname.length() != 0 &&Slname.length() != 0 && Sphone_num.length() != 0 && Scompanyname.length() != 0 && Slicence_nu.length() != 0 && Scarplate.length() != 0 && Senterpass.length() != 0 && Sconfirmpass.length() != 0 )
+            Sfname = fname.getText().toString();
+            Slname = lname.getText().toString();
+            Sphone_num = phone_num.getText().toString();
+            Scompanyname = companyname.getText().toString();
+            Slicence_nu = licence_nu.getText().toString();
+            Scarplate = carplate.getText().toString();
+            Senterpass = enterpass.getText().toString();
+            Sconfirmpass = confirmpass.getText().toString();
+            if(Sfname.length() > 0 && Slname.length() > 0 && Sphone_num.length() > 0 && Scompanyname.length() > 0 && Slicence_nu.length() > 0 && Scarplate.length() > 0 && Senterpass.length() > 0 && Sconfirmpass.length() > 0 )
             {
-                Toast.makeText(Signup.this, "Please Fill all fields", Toast.LENGTH_SHORT).show();
-                if(enterpass.equals(confirmpass))
+                Toast.makeText(Signup.this, "Ready", Toast.LENGTH_SHORT).show();
+                if(Senterpass.equals(Sconfirmpass))
                 {
                     Toast.makeText(Signup.this, "You can Create an Account", Toast.LENGTH_SHORT).show();
                 }
